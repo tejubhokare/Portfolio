99619755cf2ed8489eae816e400810a3c5bb3732
Tejaswi Bhokare <tejaswibhokare17@gmail.com>
2024-09-12 17:02:42 -0400

    Added profile image into hero section and modified styling.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { HERO_CONTENT } from "../constants";
+import profilepic from "../assets/ProfileImg.jpg";
 
 const Hero = () => {
     return (
-        <div className='border-b border-neutral-900 pb-4 lg:mb-35'>
+        <div className='border-b border-neutral-900 pb-4 lg:mb-35' style={{ marginTop: '-4%' }}>
             <div className='flex flex-wrap'>
                 <div className='w-full lg:w-1/2'>
                     <div className='flex flex-col items-center lg:items-start'>
@@ -17,9 +18,15 @@ const Hero = () => {
                         <p className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</p>
                     </div>
                 </div>
+                <div className='w-full lg:w-1/2 lg:p-8'>
+                    <div className='flex justify-center'>
+                        <img src={profilepic} alt='Tejaswi' width="70%" height="70%" style={{ borderRadius: '6%' }} ></img>
+
+                    </div>
+                </div>
             </div>
         </div>
-    )
-}
-
+    );
+};
+//width="80%" height="80%" style={{ borderRadius: '6%', marginTop: '-12%' }}
 export default Hero
